e3361863fc15ad352874a56249fc3a131a25dd3e
Nicolo Contin <nicocontin@gmail.com>
2023-05-04 15:35:08 +0200

    almost done with project, need to polish comments

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,6 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-/* router.get("/main", isLoggedIn, (req, res, next) => {
-  console.log(req.session);
-  //render view and user is coming from req.session.user
-  res.render("main", { user: req.session.user });
-}); */
-
-router.get("/private", isLoggedIn, (req, res, next) => {
-  console.log(req.session);
-  //render view and user is coming from req.session.user
-  res.render("private", { user: req.session.user });
-});
-
 router.post("/logout", (req, res, next) => {
   req.session.destroy((err) => {
     if (err) next(err);
